9993c1181b157ef5a50246847f83027c06bcf7cb
Jose Jimenez <josejcamara@gmail.com>
2024-02-10 20:20:14 +0000

    aws-scripts first commit

---
+++ aws_scripts/README.md
@@ -0,0 +1,8 @@
+# AWS scripts with utils
+
+Few useful python scripts to use over AWS
+
+### Prepare environment
+> python -m venv venv  
+> source venv/bin/activate  
+> pip install -r requirements.txt  
---
+++ aws_scripts/spreadsheet.py
@@ -0,0 +1,56 @@
+# pylint: disable=import-error
+# pyright: reportMissingImports=false, reportMissingModuleSource=false
+
+""" Manage Spreadsheets """
+
+import openpyxl
+
+
+class Spreadsheet:
+    """ Excel Spreadsheet Management """
+
+    def __init__(self):
+        # Creats empty workbook
+        wb = openpyxl.Workbook()
+
+        self.wb = wb
+        self.sheets = {}
+        self.data = {}
+
+    def add_sheet(self, sheet_id, data=None):
+        """ Add Sheet with optional data """
+        if data is None:
+            data = []
+        sheet = self.wb.create_sheet(sheet_id)
+
+        self.sheets[sheet_id] = sheet
+        self.data[sheet_id] = data
+
+        return sheet
+
+    def set_sheet_data(self, sheet_id, data):
+        """ Set the spreadsheet data """
+        self.data[sheet_id] = data
+
+    def save(self, filename):
+        """ Save to a file """
+        for sid, sheet in self.sheets.items():
+            for row in self.data[sid]:
+                sheet.append(row)
+
+        self.wb.save(filename)
+
+
+if __name__ == "__main__":
+    xls1 = Spreadsheet()
+
+    print('Enter the demo filename to create:')
+    namefile = input()
+
+    XID1 = "sheet1"
+    xls1.add_sheet(XID1)
+
+    data1 = [["Title1", "Title2"], ["row1_col1", "row1_col2"]]
+    xls1.set_sheet_data(XID1, data1)
+
+    xls1.save(namefile)
